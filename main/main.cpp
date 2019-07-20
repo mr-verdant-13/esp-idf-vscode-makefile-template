10385119eb07238949d7e164d6d7d9938c0a8ec1
Karlo Verde <karlo.verde.1996@gmail.com>
2019-07-19 21:29:01 -0500

    test code included

--- main/main.cpp
+++ main/main.cpp
@@ -1,19 +1,5 @@
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "esp_log.h"
-
 extern "C"
 {
+#include "test/test0000.cpp"
 	void app_main();
-}
-
-static const char *TAG = "TEST_0000";
-
-void app_main(void)
-{
-	for (;;)
-	{
-		ESP_LOGI(TAG, "Loop");
-		vTaskDelay(1000);
-	}
 }

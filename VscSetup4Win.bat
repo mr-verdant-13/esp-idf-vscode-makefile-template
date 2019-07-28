ae96b30af4e19aada821d7c8a41771772b49c422
Karlo Verde <karlo.verde.1996@gmail.com>
2019-07-28 01:21:58 -0500

    Setup VSC for the ESP-IDF

--- VscSetup4Win.bat
+++ VscSetup4Win.bat
@@ -1,1 +1,3 @@
-copy ".vscode\_tasks.json" ".vscode\tasks.json"
+copy ".vscode\tasks_w.json" ".vscode\tasks.json"
+copy ".vscode\settings_w.json" ".vscode\settings.json"
+copy ".vscode\c_cpp_properties_w.json" ".vscode\c_cpp_properties.json"
